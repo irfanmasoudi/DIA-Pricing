a0ee5d4372e65d01858098984ec0e3741822f695
irfanmasoudi <irfanmasoudi@users.noreply.github.com>
2021-04-03 16:12:06 +0200

    kurangi

--- jancok.py
+++ jancok.py
@@ -11,10 +11,3 @@ from Learner import *
         self.t += 1
         self.update_observations(pulled_arm, reward)
         self.expected_payoffs[pulled_arm] = (self.expected_payoffs[pulled_arm]*(self.t - 1.0) + reward)/self.t
-
-    def pull_arm(self):
-        if self.t < self.n_arms:
-            return self.t
-        idxs = np.argwhere(self.expected_payoffs == self.expected_payoffs.max()).reshape(-1)
-        pulled_arm = np.random.choice(idxs)
-        return pulled_arm
