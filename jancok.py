def56d19378d1e302fc727a60e37a7e00e39c4dd
irfanmasoudi <irfanmasoudi@users.noreply.github.com>
2021-04-03 16:33:09 +0200

    revert

--- jancok.py
+++ jancok.py
@@ -1,13 +1,3 @@
 import numpy as np
 from Learner import *
- 
 
- class Greedy_Learner(Learner):
-    def __init__(self, n_arms):
-        super().__init__(n_arms)
-        self.expected_payoffs = np.zeros(n_arms)
-
-    def update(self, pulled_arm, reward):
-        self.t += 1
-        self.update_observations(pulled_arm, reward)
-        self.expected_payoffs[pulled_arm] = (self.expected_payoffs[pulled_arm]*(self.t - 1.0) + reward)/self.t
